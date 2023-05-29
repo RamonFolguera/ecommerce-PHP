0b70fd6187a900d5f2b5b7ebb1fe768e04a1602f
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-29 16:56:56 +0200

    chores: refactored footer and header to a separate file and added required where needed. working on register file

--- shopping/cart.php
+++ shopping/cart.php
@@ -12,52 +12,7 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
-    <div class="container" style="margin-top: none">
-        <a class="navbar-brand  text-white" href="#">Bookstore</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form> -->
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item ">
-            <a class="nav-link  text-white" href="http://localhost/bookstore/contact.php">Contact</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href="http://localhost/bookstore/shopping/cart.php"><i class="fas fa-shopping-cart"></i>(2)</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href="http://localhost/bookstore/categories/index.php">Categories</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle  text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Username
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  text-white" href="#">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  text-white" href="#">Register</a>
-            </li>
-        </ul>
-       
-        </div>
-    </div>
-    </nav>
+  
 
     <div class="container">   
     <div class="row d-flex justify-content-center align-items-center h-100 mt-5 mt-5">
